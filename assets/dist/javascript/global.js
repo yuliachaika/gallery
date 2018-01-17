bf87f2d19ed4cd8765fa81859d6657d113fd5006
Yulia <yulia.chaika@icloud.com>
2018-01-17 13:43:57 +0200

    add btn to top

--- assets/dist/javascript/global.js
+++ assets/dist/javascript/global.js
@@ -10259,6 +10259,28 @@ return jQuery;
 (function($){
     
     $(document).ready( function() {
+
+      //--scroll to top 
+      $(function (){
+        $("#back-top").hide();
+      
+        $(window).scroll(function (){
+          if ($(this).scrollTop() > 100){
+            $("#back-top").fadeIn();
+          } else{
+            $("#back-top").fadeOut();
+          }
+        });
+
+        $("#back-top a").click(function (){
+          $("body,html").animate({
+            scrollTop:0
+          }, 800);
+          return false;
+        });
+      });
+
+      //tabs
         $('.three-col-nav__link').on('click', function(e) {
           e.preventDefault();
           var element = $(this);
--- assets/src/javascript/global.js
+++ assets/src/javascript/global.js
@@ -3,6 +3,28 @@
 (function($){
     
     $(document).ready( function() {
+
+      //--scroll to top 
+      $(function (){
+        $("#back-top").hide();
+      
+        $(window).scroll(function (){
+          if ($(this).scrollTop() > 100){
+            $("#back-top").fadeIn();
+          } else{
+            $("#back-top").fadeOut();
+          }
+        });
+
+        $("#back-top a").click(function (){
+          $("body,html").animate({
+            scrollTop:0
+          }, 800);
+          return false;
+        });
+      });
+
+      //tabs
         $('.three-col-nav__link').on('click', function(e) {
           e.preventDefault();
           var element = $(this);
